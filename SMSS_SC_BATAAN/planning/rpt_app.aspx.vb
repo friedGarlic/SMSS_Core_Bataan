68a030590b4132b52817fa160be622cafbf36f07
Devworks20 <devworks20@gmail.com>
2025-05-30 16:05:16 +0800

    MJ-ReportsUpdate-053025

--- SMSS_SC_BATAAN/planning/rpt_app.aspx.vb
+++ SMSS_SC_BATAAN/planning/rpt_app.aspx.vb
@@ -11,25 +11,27 @@ Partial Class rpt_app
         Me.Session("view") = "0"
         Select Case Session("BAC Members")
             Case "seven"
-                'rbFormat.SelectedValue = 2
-
+                If Not IsPostBack Then
+                    rbFormat.SelectedValue = 4
+                End If
                 rbFormat.Items(0).Enabled = True    ' Enaable value 0
                 rbFormat.Items(1).Enabled = False   ' Disable value 1
                 rbFormat.Items(3).Enabled = False   ' Disable value 3
                 rbFormat.Items(2).Enabled = True    ' Enable value 2
                 rbFormat.Items(4).Enabled = True    ' Enable value 4
-                LoadRbChoice()
-            Case "five"
-                'rbFormat.SelectedValue = 1
 
+            Case "five"
+                If Not IsPostBack Then
+                    rbFormat.SelectedValue = 3
+                End If
                 rbFormat.Items(0).Enabled = True    ' Enaable value 0
                 rbFormat.Items(1).Enabled = True    ' Enable value 1
                 rbFormat.Items(3).Enabled = True    ' Enable value 3
                 rbFormat.Items(2).Enabled = False   ' Disable value 2
                 rbFormat.Items(4).Enabled = False   ' Disable value 4
-                LoadRbChoice()
-        End Select
 
+        End Select
+        LoadRbChoice()
 
     End Sub
     Protected Sub LinkButton1_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles LinkButton1.Click
@@ -77,38 +79,40 @@ Partial Class rpt_app
         'End If
         Me.CrystalReportViewer3.ToolPanelView = CrystalDecisions.Web.ToolPanelViewType.None
 
+        Me.CrystalReportViewer3.ReportSource = Nothing ' clear old report
+
         Select Case Session("BAC Members")
             Case "seven"
                 If rbFormat.SelectedItem.Value = 2 Then
-                    Me.CrystalReportSource1.Report.FileName = "rpt_app_GPPB_LGU_v4.rpt"
-                    Me.CrystalReportViewer3.ReportSource = Me.CrystalReportSource1
+                    Me.CrystalReportSource1.Report.FileName = "rpt_app_GPPB_LGU_v6.rpt"
                     Me.CrystalReportSource1.ReportDocument.SetDatabaseLogon(objDerived.username, objDerived.Password)
                     Me.CrystalReportSource1.ReportDocument.SetParameterValue("@CYear", Session("year"))
                     Me.CrystalReportSource1.ReportDocument.SetParameterValue("@isContinuing", Session("isContinuing"))
                     Me.CrystalReportSource1.ReportDocument.SetParameterValue("@isSupplemental", Session("isSupplemental"))
+                    Me.CrystalReportViewer3.ReportSource = Me.CrystalReportSource1
                 ElseIf rbFormat.SelectedItem.Value = 4 Then
-                    Me.CrystalReportSource2.Report.FileName = "app_cagayan_nonCSE_Updated_v2.rpt"
-                    Me.CrystalReportViewer3.ReportSource = Me.CrystalReportSource2
+                    Me.CrystalReportSource2.Report.FileName = "app_cagayan_nonCSE_Updated_v4.rpt"
                     Me.CrystalReportSource2.ReportDocument.SetDatabaseLogon(objDerived.username, objDerived.Password)
                     Me.CrystalReportSource2.ReportDocument.SetParameterValue("@CYear", Session("year"))
                     Me.CrystalReportSource2.ReportDocument.SetParameterValue("@isContinuing", Session("isContinuing"))
                     Me.CrystalReportSource2.ReportDocument.SetParameterValue("@isSupplemental", Session("isSupplemental"))
+                    Me.CrystalReportViewer3.ReportSource = Me.CrystalReportSource2
                 End If
             Case "five"
                 If rbFormat.SelectedItem.Value = 1 Then
-                    Me.CrystalReportSource1.Report.FileName = "rpt_app_GPPB_LGU.rpt"
-                    Me.CrystalReportViewer3.ReportSource = Me.CrystalReportSource1
+                    Me.CrystalReportSource1.Report.FileName = "rpt_app_GPPB_LGU_v5.rpt"
                     Me.CrystalReportSource1.ReportDocument.SetDatabaseLogon(objDerived.username, objDerived.Password)
                     Me.CrystalReportSource1.ReportDocument.SetParameterValue("@CYear", Session("year"))
                     Me.CrystalReportSource1.ReportDocument.SetParameterValue("@isContinuing", Session("isContinuing"))
                     Me.CrystalReportSource1.ReportDocument.SetParameterValue("@isSupplemental", Session("isSupplemental"))
+                    Me.CrystalReportViewer3.ReportSource = Me.CrystalReportSource1
                 ElseIf rbFormat.SelectedItem.Value = 3 Then
-                    Me.CrystalReportSource2.Report.FileName = "app_cagayan_nonCSE_Updated.rpt"
-                    Me.CrystalReportViewer3.ReportSource = Me.CrystalReportSource2
+                    Me.CrystalReportSource2.Report.FileName = "app_cagayan_nonCSE_Updated_v3.rpt"
                     Me.CrystalReportSource2.ReportDocument.SetDatabaseLogon(objDerived.username, objDerived.Password)
                     Me.CrystalReportSource2.ReportDocument.SetParameterValue("@CYear", Session("year"))
                     Me.CrystalReportSource2.ReportDocument.SetParameterValue("@isContinuing", Session("isContinuing"))
                     Me.CrystalReportSource2.ReportDocument.SetParameterValue("@isSupplemental", Session("isSupplemental"))
+                    Me.CrystalReportViewer3.ReportSource = Me.CrystalReportSource2
                 End If
         End Select
     End Sub
